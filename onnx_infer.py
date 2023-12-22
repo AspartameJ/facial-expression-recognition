c36b5fc3ba5823cd057b7ff7be9e96a07941f714
AspartameJ <1054136235@qq.com>
2023-12-22 15:26:34 +0800

    onnx infer

--- onnx_infer.py
+++ onnx_infer.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("./")
 import torch
 import onnxruntime as rt
 import torch.onnx
@@ -6,8 +8,15 @@ import torch.nn.functional as F
 import cv2
 import numpy as np
 from PIL import Image
-from .model import transforms
+from model import transforms
 
+
+def softmax(x):
+    if len(x.shape) > 1:
+        x = np.exp(x) / np.sum(np.exp(x),axis=1).reshape(-1,1)
+    else:
+        x = np.exp(x) / np.sum(np.exp(x))
+    return x
 
 def test_onnx():
     transform_test = transforms.Compose([
@@ -15,11 +24,9 @@ def test_onnx():
                 transforms.Lambda(lambda crops: torch.stack(
                     [transforms.ToTensor()(crop) for crop in crops])),
             ])
-    img_paths = ["./imgs/facial_expression_recognition.jpg"]
+    img_paths = ["./imgs/1.jpg", "./imgs/2.jpg"]
 
     sess = rt.InferenceSession("./model/vgg19.onnx", providers=['CPUExecutionProvider'])
-    #  sess = rt.InferenceSession(onnx_sim, providers=['CUDAExecutionProvider'])  # providers=[CPUExecutionProvider,'CUDAExecutionProvider']
-    #  providers=[CPUExecutionProvider,'CUDAExecutionProvider']
 
     input_name = sess.get_inputs()[0].name
     out_name = sess.get_outputs()[0].name
@@ -35,7 +42,7 @@ def test_onnx():
     
     for img_path in img_paths:
         img = cv2.imread(img_path)
-        img = cv2.cvtColor(img.cpu().numpy(), cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = cv2.resize(img, (48, 48))
         img = img[:, :, np.newaxis]
         img = np.concatenate((img, img, img), axis=2)
@@ -43,17 +50,14 @@ def test_onnx():
         img = Image.fromarray(np.uint8(img))
         inputs = transform_test(img)
 
-        ncrops, c, h, w = inputs.shape
+        inputs = inputs.numpy()
+        outputs = sess.run([out_name], {input_name:inputs})
+        outputs_avg = outputs[0].mean(0)    # avg over crops
 
-        inputs = inputs.view(-1, c, h, w)
-        inputs = inputs.to('cpu')
-        inputs = Variable(inputs, volatile=True)
-        outputs = sess(inputs)
-        outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
-
-        score = F.softmax(outputs_avg)
-        _, predicted = torch.max(outputs_avg.data, 0)
-        return predicted, score
+        score = softmax(outputs_avg)
+        predicted = np.argmax(outputs_avg)
+        print(predicted, score)
 
 if __name__ == '__main__':
-    print(test_onnx())
+    test_onnx()
+
