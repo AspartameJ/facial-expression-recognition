3668b6640a8c5c1ad4d210422143be4c474a8304
AspartameJ <1054136235@qq.com>
2023-12-22 13:43:33 +0800

    修改om保存地址

--- tools/onnx2om.sh
+++ tools/onnx2om.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-ONNX_PATH=../model/vgg19.onnx
+ONNX_DIR=../model
 SOC_VERSION=`python3 -c 'import acl; print(acl.get_soc_name())'`
-ONNX_NAME=`basename $ONNX_PATH`
+ONNX_NAME=vgg19.onnx
 MODEL_NAME=${ONNX_NAME%%.*}
 PRECISION_MODE=force_fp16
 
-atc --model $ONNX_PATH --framework 5 --output ${MODEL_NAME}_${PRECISION_MODE} --soc_version $SOC_VERSION --precision_mode $PRECISION_MODE
+atc --model $ONNX_DIR/$ONNX_NAME --framework 5 --output ONNX_DIR/${MODEL_NAME}_${PRECISION_MODE} --soc_version $SOC_VERSION --precision_mode $PRECISION_MODE
