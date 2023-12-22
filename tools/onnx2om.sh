5828383872dd5e349786d679b78ffc3cc80b94e7
AspartameJ <1054136235@qq.com>
2023-12-22 16:57:28 +0800

    修改模型保存路径

--- tools/onnx2om.sh
+++ tools/onnx2om.sh
@@ -6,4 +6,4 @@ ONNX_NAME=vgg19.onnx
 MODEL_NAME=${ONNX_NAME%%.*}
 PRECISION_MODE=force_fp16
 
-atc --model $ONNX_DIR/$ONNX_NAME --framework 5 --output ONNX_DIR/${MODEL_NAME}_${PRECISION_MODE} --soc_version $SOC_VERSION --precision_mode $PRECISION_MODE
+atc --model $ONNX_DIR/$ONNX_NAME --framework 5 --output $ONNX_DIR/${MODEL_NAME}_${PRECISION_MODE} --soc_version $SOC_VERSION --precision_mode $PRECISION_MODE
