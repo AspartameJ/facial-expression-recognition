1c5bc6278973f7634af54be536b914b6aaeb6d3e
AspartameJ <1054136235@qq.com>
2023-12-22 15:26:18 +0800

    10, 3, 44, 44

--- tools/exporter.py
+++ tools/exporter.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
      pretrained_dict = torch.load("../model/pytorch_model.pt", map_location=torch.device('cpu'))
      model.load_state_dict(pretrained_dict['net'], strict=True)
      model.eval()
-     dummy_input = torch.randn(1, 3, 48, 48)
+     dummy_input = torch.randn(10, 3, 44, 44)
      torch.onnx.export(model, dummy_input, "../model/vgg19.onnx", opset_version=11, input_names=["input"], output_names=["output"], verbose=True)
